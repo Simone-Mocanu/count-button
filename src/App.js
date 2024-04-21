f350759d3508667e4fd9592b46e4a0d0a62e711c
beji <simone0443@outlook.com>
2024-04-21 17:18:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center m-5">
+      <Button variant="contained">
+        <div className="font-bold ">Press me!</div>
+      </Button>
     </div>
   );
 }
