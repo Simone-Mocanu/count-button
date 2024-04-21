43a798603e416d02df23110a768527648b52dba5
beji <simone0443@outlook.com>
2024-04-21 17:36:29 +0300

    add button functionality and count

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="flex items-center justify-center m-5">
-      <Button variant="contained">
-        <div className="font-bold ">Press me!</div>
-      </Button>
+    <div className="flex items-center justify-center m-4">
+        <div className="grid grid-cols-1">
+        <div className="flex justify-center">
+          <Button variant="contained" onClick={() => {setCount(count + 1)}}>
+            <span className="font-bold">Press me!</span>
+          </Button>
+        </div>
+        <div className="mt-5 flex justify-center">
+          <Typography variant="h4">{ count }</Typography>
+        </div>
+      </div>
     </div>
   );
 }
